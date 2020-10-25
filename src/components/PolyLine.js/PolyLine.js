3e37f281c3071c54a5fef61f968ef9c6e3072ac2
asafcc <asaf995@gmail.com>
2020-10-25 10:14:12 +0200

    file structure

--- src/components/PolyLine.js/PolyLine.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-import { Polyline } from "react-leaflet";
-
-function PolyLine({ polyLine }) {
-  return <>{<Polyline color="blue" positions={polyLine} />}</>;
-}
-
-export default PolyLine;
