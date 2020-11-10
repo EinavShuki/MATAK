e88b32eeb55802972d34905f815d1ef4d76ee9e4
Einav1 <einavshpigel@gmail.com>
2020-11-10 09:41:16 +0200

    add Reporting component-doesn't have css yet

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -4,15 +4,19 @@ import { CgCloseO } from "react-icons/cg";
 import {
   OPTIONS_PAGE,
   ROUTE_DETAILS_PAGE,
+  REPORTING_PAGE,
 } from "../../constants/pageConstants";
+
+//INITIAL COMPONENTS
 import Options from "./SideMenuComponents/Options";
 import RouteDetails from "./SideMenuComponents/RouteDetails";
+import Reporting from "./SideMenuComponents/Reporting"
 
 function SideMenu({ isOpen, closeSideMenu }) {
   const [page, setPage] = useState({
     open: OPTIONS_PAGE,
   });
-console.log("hi");
+
   return (
     <div className={isOpen ? "frosted nav-menu active" : "frosted nav-menu"}>
       <span id="close-button" onClick={() => closeSideMenu(false)}>
@@ -21,6 +25,7 @@ console.log("hi");
       <div id="menu-div">
         {page.open === OPTIONS_PAGE && <Options setPage={setPage} />}
         {page.open === ROUTE_DETAILS_PAGE && <RouteDetails />}
+        {page.open === REPORTING_PAGE && <Reporting />}
       </div>
     </div>
   );
