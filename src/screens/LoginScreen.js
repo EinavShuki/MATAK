db0ee8b7adc87af287e91b0bd854a961b47fabfe
ron <nrdronys@gmail.com>
2020-11-28 22:46:15 +0200

    works nice

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import MatakIcon from "../images/matak.png";
+import MatakModal from "./MatakModal";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -39,14 +40,18 @@ export default function LoginScreen({history}) {
     const [isPasswordValid, setIsPasswordValid] = useState(false);
     const [usernameLength, setUsernameLength] = useState(0);
     const [passwordLength, setPasswordLength] = useState(0);
+    const [modalText, setModalText] = useState('');
+    const [showModal, setShowModal] = useState(false);
 
     const handleClick = () => {
         if (isPasswordValid && isUsernameValid) {
             history.push("/home");
         } else if (!isUsernameValid) {
-            alert('User name is not valid');
+            setShowModal(true);
+            setModalText('User name is not valid');
         } else if (!isPasswordValid) {
-            alert('Password is not valid');
+            setShowModal(true);
+            setModalText('Password is not valid');
         }
     };
     const classes = useStyles();
@@ -64,8 +69,9 @@ export default function LoginScreen({history}) {
     };
 
     return (
-        <Container id="login" component="main" maxWidth="xs">
+        <Container id="login" component="main" maxWidth="xs" onClick={() => showModal && setShowModal(false)}>
             <div className={classes.paper}>
+                <MatakModal text={modalText} show={showModal}/>
                 <div className={classes.headline}>
                     <Typography component="h1" variant="h3">
                         Sign in
@@ -83,7 +89,6 @@ export default function LoginScreen({history}) {
                                autoComplete="current-password"
                     />
                     <Button
-                        type="submit"
                         fullWidth
                         variant="contained"
                         color="primary"
---
+++ src/screens/MatakModal.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        borderRadius: '5px',
+        outline: 'none',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+export default function MatakModal({text, show}) {
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+
+    return (
+        <div>
+            <Modal
+                open={show}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div style={modalStyle} className={classes.paper}>
+                    <p id="simple-modal-title">{text}</p>
+                </div>
+            </Modal>
+        </div>
+    );
+}
