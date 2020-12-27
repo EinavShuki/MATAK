5fa59cc31a00e26c5d04f8c4bdc20abd6afd565f
Einav1 <einavshpigel@gmail.com>
2020-12-27 21:16:45 +0200

    adding input option to my_account

--- src/screens/MyAccountScreen.js
+++ src/screens/MyAccountScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -9,61 +9,144 @@ import Avatar from "@material-ui/core/Avatar";
 
 import NavBar from "../components/NavBar";
 import { MdVpnKey, MdEmail, MdLocalPhone } from "react-icons/md";
+import { Button } from "@material-ui/core";
 
 function MyAccountScreen() {
-  function enableChnage() {}
+  const [selctedPhone, setSelctedPhone] = useState(false);
+  const [selctedMail, setSelctedMail] = useState(false);
+  const [selctedPassword, setSelctedPassword] = useState(false);
   return (
     <>
       <NavBar />
       <div className="frame">
         <div className="user-details">
           <h1 className="name">Einav Shpigel</h1>
-          <Avatar className="avatar_name" >ES</Avatar>
+          <Avatar className="avatar_name">ES</Avatar>
           <h3 className="organization-name">
             here we put the organization name
           </h3>
           <List component="nav" aria-label="main mailbox folders">
-            <ListItem button>
-              <ListItemIcon>
-                <MdEmail />
-              </ListItemIcon>
-              <ListItemText primary="User Email" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <MdLocalPhone />
-              </ListItemIcon>
-              <ListItemText primary="User Phone" />
-            </ListItem>
-            <ListItem button>
-              <ListItemIcon>
-                <MdVpnKey />
-              </ListItemIcon>
-              <ListItemText primary="Change password" />
-            </ListItem>
+            {selctedMail ? (
+              <ListItem input onFocus={() => {setSelctedPhone(false);setSelctedPassword(false);}}>
+                <ListItemIcon>
+                  <MdEmail />
+                </ListItemIcon>
+                <TextField
+                  id="standard-basic"
+                  label="Standard"
+                  margin="normal"
+                  name="Mail"
+                  label="User Email"
+                  type="email"
+                  autoComplete="off"
+                  autoFocus
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    alignSelf: "flex-end",
+                    padding: "0.5rem 0.5rem",
+                    margin: "0.5rem 0",
+                  }}
+                  onClick={() => setSelctedMail(false)}
+                >
+                  Done
+                </Button>
+              </ListItem>
+            ) : (
+              <ListItem button onClick={() => setSelctedMail(true)}>
+                <ListItemIcon>
+                  <MdEmail />
+                </ListItemIcon>
+                <ListItemText primary="User Email" />
+              </ListItem>
+            )}
+
+            {!selctedPhone ? (
+              <ListItem button onClick={() => setSelctedPhone(true)}>
+                <ListItemIcon>
+                  <MdLocalPhone />
+                </ListItemIcon>
+                <ListItemText primary="User Phone" />
+              </ListItem>
+            ) : (
+              <ListItem input onFocus={() => {setSelctedMail(false);setSelctedPassword(false);}}>
+                <ListItemIcon>
+                  <MdLocalPhone />
+                </ListItemIcon>
+                <TextField
+                  id="standard-basic"
+                  label="Standard"
+                  margin="normal"
+                  name="Mail"
+                  label="User Phone"
+                  type="tel"
+                  autoComplete="off"
+                  autoFocus
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    alignSelf: "flex-end",
+                    padding: "0.5rem 0.5rem",
+                    margin: "0.5rem 0",
+                  }}
+                  onClick={() => setSelctedPhone(false)}
+                >
+                  Done
+                </Button>
+              </ListItem>
+            )}
+            {!selctedPassword ? (
+              <ListItem button onClick={() => setSelctedPassword(true)}>
+                <ListItemIcon>
+                  <MdVpnKey />
+                </ListItemIcon>
+                <ListItemText primary="Change Password" />
+              </ListItem>
+            ) : (
+              <ListItem input onFocus={() => {setSelctedPhone(false);setSelctedMail(false);}}>
+                <ListItemIcon>
+                  <MdVpnKey />
+                </ListItemIcon>
+                <TextField
+                  id="standard-basic"
+                  label="Standard"
+                  margin="normal"
+                  name="Mail"
+                  label="Password"
+                  type="password"
+                  autoComplete="off"
+                  autoFocus
+                />
+                {"  "}
+                <TextField
+                  id="standard-basic"
+                  label="Standard"
+                  margin="normal"
+                  name="Mail"
+                  label="Conform Password"
+                  type="password"
+                  autoComplete="off"
+                  autoFocus
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    alignSelf: "flex-end",
+                    padding: "0.5rem 0.5rem",
+                    margin: "0.5rem 0",
+                  }}
+                  onClick={() => setSelctedPassword(false)}
+                >
+                  Done
+                </Button>
+              </ListItem>
+            )}
           </List>
-          {/* SHOULD BE ENABLED ONLY IF  chang password BUTTON WAS PRESSED*/}
-          <div className="change-password">
-            <TextField
-              variant="outlined"
-              margin="normal"
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="off"
-              autoFocus
-            />{" "}
-            <TextField
-              variant="outlined"
-              type="password"
-              margin="normal"
-              id="confirm-password"
-              label="Confirm Password"
-              name="confirm-password"
-              autoComplete="off"
-            />
-          </div>
         </div>
       </div>
     </>
