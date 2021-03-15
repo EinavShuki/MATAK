2cefc7bc193498e32f3a9969f9b41c5d55cc8cbd
Stav Elmashally <stavelmashally6@gmail.com>
2021-03-15 16:33:51 +0200

    connect redux-form

--- src/components/AdminScreen/UserEditForm.js
+++ src/components/AdminScreen/UserEditForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import { MdVpnKey, MdEmail, MdLocalPhone, MdAccountCircle, MdCardTravel, MdPieChart } from "react-icons/md";
+import { MdEmail, MdLocalPhone, MdAccountCircle, MdCardTravel, MdPieChart } from "react-icons/md";
 import {InputAdornment } from "@material-ui/core";
 
 function UserEditForm({
