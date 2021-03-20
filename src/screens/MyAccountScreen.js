6edacce9ed10e35cba636f5ea467407defc6c5af
Einav1 <einavshpigel@gmail.com>
2021-03-20 21:10:28 +0200

    working on API  connecting

--- src/screens/MyAccountScreen.js
+++ src/screens/MyAccountScreen.js
@@ -30,102 +30,115 @@ function MyAccountScreen() {
   return (
     <>
       <NavBar />
-      <div className="frame">
-        <h1
-          // value={currentUser.name}
-          className="name"
-        >
-          Einav Shpigel
-        </h1>
-        <Avatar className="avatar_name">ES</Avatar>
-        <h3 className="organization-name" style={{ marginLeft: "6rem" }}>
-          here we put the organization name
-        </h3>
-
-        <List
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: "0 2rem",
-          }}
-        >
-          <ListItem>
-            <TextField
-              fullWidth
-              margin="dense"
-              name="Mail"
-              label="User Email"
-              type="email"
-              autoComplete="off"
-              autoFocus
-              // value={currentUser.email}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <MdEmail />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </ListItem>
-          <ListItem>
-            <TextField
-              fullWidth
-              margin="normal"
-              name="Phone"
-              label="User Phone"
-              type="tel"
-              // value={currentUser.phone}
-              autoComplete="off"
-              autoFocus
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <MdLocalPhone />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </ListItem>
-          <ListItem>
-            <TextField
-              fullWidth
-              margin="normal"
-              name="password"
-              label="Password"
-              type="password"
-              autoComplete="off"
-              autoFocus
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <MdVpnKey />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </ListItem>
-          <ListItem>
-            <TextField
-              fullWidth
-              margin="normal"
-              name="confirm-password"
-              label="Confirm Password"
-              type="password"
-              autoComplete="off"
-              autoFocus
-            />
-          </ListItem>
-          <Button
-            variant="contained"
-            color="primary"
+      <div className="my_account_screen">
+        <div className="frame">
+          <span
             style={{
               display: "flex",
-              justifyContent: "center",
               alignItems: "center",
+              justifyContent: "center",
             }}
-          ></Button>
-        </List>
+          >
+            <Avatar className="avatar_name">ES</Avatar>
+            <h1
+              // value={currentUser.name}
+              className="name"
+            >
+              Einav Shpigel
+            </h1>
+          </span>
+          <h3 className="organization-name">
+            here we put the organization name
+          </h3>
+
+          <List
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "0 2rem",
+            }}
+          >
+            <ListItem>
+              <TextField
+                fullWidth
+                margin="dense"
+                name="Mail"
+                label="User Email"
+                type="email"
+                autoComplete="off"
+                autoFocus
+                // value={currentUser.email}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MdEmail />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </ListItem>
+            <ListItem>
+              <TextField
+                fullWidth
+                margin="normal"
+                name="Phone"
+                label="User Phone"
+                type="tel"
+                // value={currentUser.phone}
+                autoComplete="off"
+                autoFocus
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MdLocalPhone />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </ListItem>
+            {/* <ListItem>
+              <TextField
+                fullWidth
+                margin="normal"
+                name="password"
+                label="Password"
+                type="password"
+                autoComplete="off"
+                autoFocus
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MdVpnKey />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </ListItem>
+            <ListItem>
+              <TextField
+                fullWidth
+                margin="normal"
+                name="confirm-password"
+                label="Confirm Password"
+                type="password"
+                autoComplete="off"
+                autoFocus
+              />
+            </ListItem> */}
+            <Button
+              variant="contained"
+              color="primary"
+              style={{
+                alignSelf: "center",
+                marginTop: "1rem",
+                width: "90%",
+                padding: "0.5rem 0.5rem",
+              }}
+            >
+              Update
+            </Button>
+          </List>
+        </div>
       </div>
     </>
   );
