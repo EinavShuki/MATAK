ff86f8af590b64988ff50c1e55b2b059044bc147
asafcc <asaf995@gmail.com>
2021-03-24 22:26:07 +0200

    routes with api

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users";
 import createdRouteReducer from "./createdRoute";
+import userRoutesReducer from "./userRoutes";
 
 export default configureStore({
   reducer: {
     users: usersReducer,
     createdRoute: createdRouteReducer,
+    userRoutes: userRoutesReducer,
   },
 });
