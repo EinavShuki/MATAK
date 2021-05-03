7e4673a4958dc93bcccba6792badef20f59f8033
ronys <nrdronys@gmail.com>
2021-05-03 10:40:39 +0300

    First form version

--- src/components/RoutesManagment/RouteForm.js
+++ src/components/RoutesManagment/RouteForm.js
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import {makeStyles} from '@material-ui/core/styles';
 import TextField from "@material-ui/core/TextField/TextField";
 import {Button} from "@material-ui/core";
 import "./RoutesForm.css";
 import {MdClose} from "react-icons/md";
 import DatePicker from "../DatePicker/DatePicker";
+import {useSelector} from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,6 +21,17 @@ export default function RouteForm({routeFormData, handleFormSubmit, onClose}) {
     const classes = useStyles();
     const [routeData, setRouteData] = useState(routeFormData.route);
 
+    const {
+        currentUser: { isAdminOrMatakUser },
+    } = useSelector(state => state.users);
+
+    const permissions = useMemo(
+        () => ({
+            isDisabled: routeData.Status_Name !== "Changes-Required",
+        }),
+        []
+    );
+
     function handleSubmit(event) {
         event.preventDefault();
         console.log(event);
@@ -27,48 +39,64 @@ export default function RouteForm({routeFormData, handleFormSubmit, onClose}) {
     }
 
     const isViewOnly = () => {
+        console.log(isAdminOrMatakUser)
         return routeFormData.type === "view";
     };
 
     return (
-        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit} style={{maxHeight: "88vh", overflowY: "auto"}}>
             <MdClose onClick={onClose} style={{position: "absolute", right: 0, top: 0, marginTop: "20px", marginRight: "20px", cursor: "pointer"}} size={24}/>
             <TextField
                 label="Route Name"
-                defaultValue={routeData["Path_Name"]}
+                defaultValue={routeData.Path_Name}
                 disabled={isViewOnly()}
             />
-            <DatePicker startingDate={new Date(routeData["Start_Date"])} setStartingDate={(date) => setRouteData({...routeData, Start_Date: date})}
-                endingDate={new Date(routeData["End_Date"])} setEndingDate={(date) => setRouteData({...routeData, End_Date: date})} isDisabled={isViewOnly()}
-            />
+            {!routeData.Is_Permanent && <DatePicker startingDate={new Date(routeData.Start_Date)} setStartingDate={(date) => setRouteData({...routeData, Start_Date: date})}
+                endingDate={new Date(routeData.End_Date)} setEndingDate={(date) => setRouteData({...routeData, End_Date: date})} isDisabled={isViewOnly() || permissions.isDisabled}
+            />}
             <TextField
                 label="Car Number"
-                defaultValue={routeData["Car_Liecene_Number"]}
+                defaultValue={routeData.Car_Liecene_Number}
                 disabled={isViewOnly()}
             />
             <TextField
                 label="Driver's Full Name"
-                defaultValue={routeData["Driver_Name"]}
+                defaultValue={routeData.Driver_Name}
                 disabled={isViewOnly()}
             />
             <TextField
                 label="Driver's Cellphone"
-                defaultValue={routeData["Driver_Cellphone"]}
+                defaultValue={routeData.Driver_Cellphone}
                 disabled={isViewOnly()}
 
+            />
+            <TextField
+                label="Terms"
+                defaultValue={routeData.Terms_Text}
+                disabled={isViewOnly()}
             />
             <TextField
                 label="Reason"
-                defaultValue={routeData["Reason_Text"]}
+                defaultValue={routeData.Reason_Text}
                 disabled={isViewOnly()}
             />
             <TextField
+                label="Remarks"
+                defaultValue={routeData.Remarks}
+                disabled={isViewOnly()}
+            />
+            {!routeData.Is_Permanent && <TextField
+                label="Status"
+                defaultValue={routeData.Status_Name}
+                disabled={isViewOnly() || !isAdminOrMatakUser}
+            />}
+            <TextField
                 label="Permanent"
-                defaultValue={routeData["Is_Permanent"]}
+                defaultValue={routeData.Is_Permanent}
                 disabled={isViewOnly()}
             />
 
-            {   !isViewOnly() && <Button
+            { !isViewOnly() && <Button
                 style={{marginTop: '20px'}}
                 fullWidth
                 type="submit"
